47ff0ab3a1860b5ee3475485298f33a8d5f839b9
Stewart Gibson <sg4654@nyu.edu>
2017-06-17 15:09:05 -0400

    Ch. 2 script update: by position

--- Scripts/Ch_2.R
+++ Scripts/Ch_2.R
@@ -85,4 +85,82 @@ Team.Fantasy.totals.Pos.2016 <- unique(Fantasy.2016[c("Week", "Team", "Pos")])
 Team.Fantasy.totals.Pos.2016$Actual.Points <- NA
 Team.Fantasy.totals.Pos.2016$Offensive.Fantasy.Points <- NA
 
+Team.Fantasy.totals.Pos.2016 <- Team.Fantasy.totals.Pos.2016[
+  which(Team.Fantasy.totals.Pos.2016$Pos == "QB" |
+    Team.Fantasy.totals.Pos.2016$Pos == "RB" |
+    Team.Fantasy.totals.Pos.2016$Pos == "WR")
+,]
 
+for (i in c(1:nrow(Team.Fantasy.totals.Pos.2016))) {
+  if (Team.Fantasy.totals.Pos.2016$Pos[i] == "QB") {
+    Team.Fantasy.totals.Pos.2016$Offensive.Fantasy.Points[i] <- sum(Fantasy.2016$DK.points[which(
+      Fantasy.2016$Team == Team.Fantasy.totals.Pos.2016$Team[i] &
+        Fantasy.2016$Week == Team.Fantasy.totals.Pos.2016$Week[i] &
+        Fantasy.2016$Pos == 'QB')], na.rm = T)} else if (
+          Team.Fantasy.totals.Pos.2016$Pos[i] == "RB") {
+          Team.Fantasy.totals.Pos.2016$Offensive.Fantasy.Points[i] <- 
+            sum(Fantasy.2016$DK.points[which(
+            Fantasy.2016$Team == Team.Fantasy.totals.Pos.2016$Team[i] &
+              Fantasy.2016$Week == Team.Fantasy.totals.Pos.2016$Week[i] &
+              Fantasy.2016$Pos == 'RB')], na.rm = T)} else {
+                Team.Fantasy.totals.Pos.2016$Offensive.Fantasy.Points[i] <- 
+                  sum(Fantasy.2016$DK.points[which(
+                    Fantasy.2016$Team == Team.Fantasy.totals.Pos.2016$Team[i] &
+                      Fantasy.2016$Week == Team.Fantasy.totals.Pos.2016$Week[i] &
+                      Fantasy.2016$Pos %in% c("WR","TE"))], na.rm = T)}
+  
+  Team.Fantasy.totals.Pos.2016$Actual.Points[i] <- Fantasy.2016$Actual.Points[which(
+    Fantasy.2016$Team == Team.Fantasy.totals.Pos.2016$Team[i] &
+      Fantasy.2016$Week == Team.Fantasy.totals.Pos.2016$Week[i])][1]
+}
+
+Team.Fantasy.totals.Pos.2016$Pos.Unit <- as.character(Team.Fantasy.totals.Pos.2016$Pos)
+Team.Fantasy.totals.Pos.2016$Pos.Unit[which(
+  as.character(Team.Fantasy.totals.Pos.2016$Pos) %in% c("WR", "TE"))] <-"WR/TE"
+
+## Visualization: Scatter plot (with regression) of Offensive Fantasy Points by positional unit
+## vs. Actual Points for all teams.  Also include regression equation in plot.
+#Fantasy.Actual.reg <- lm(data = Team.Fantasy.totals.2016, formula = Offensive.Fantasy.Points ~
+ #                          Actual.Points)
+#summary(Fantasy.Actual.reg)
+#reg.eq <- paste("Y = ", round(Fantasy.Actual.reg$coefficients[1], 2), " + ", 
+ #               round(Fantasy.Actual.reg$coefficients[2], 2), " * X", sep = '')
+
+ggplot(data = Team.Fantasy.totals.Pos.2016, aes(x = Actual.Points, y = Offensive.Fantasy.Points)) +
+  geom_point(aes(group = Pos, color = Pos), size = 0.5) + 
+  geom_smooth(aes(group = Pos, color = Pos), method = 'lm', formula = y ~ x) +
+  ggtitle("[TBD] All Teams") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  xlab("Actual Points") +
+  ylab("Total Offensive Fantasy Points") +
+  #geom_text(x = 10, y = max(Team.Fantasy.totals.Pos.2016$Offensive.Fantasy.Points), label = reg.eq,
+   #         color = 'blue', size = 5) +
+  scale_x_continuous(limits = c(-2, max(Team.Fantasy.totals.Pos.2016$Actual.Points) + 3)) +
+  scale_y_continuous(limits = c(min(Team.Fantasy.totals.Pos.2016$Offensive.Fantasy.Points - 5),
+                                max(Team.Fantasy.totals.Pos.2016$Offensive.Fantasy.Points + 5)))
+ggsave("Visualizations/Ch_2/By Position/Actual.vs.Fantasy.Points_ALL.png")
+
+## Create the same visualization as above, but for one for each of all 32 teams
+uniq.teams <- sort(unique(Team.Fantasy.totals.2016$Team))
+for (i in c(1:32)) {
+  dat <- Team.Fantasy.totals.Pos.2016[which(Team.Fantasy.totals.Pos.2016$Team == uniq.teams[i]),]
+  #Fantasy.Actual.reg <- lm(data = dat, formula = Offensive.Fantasy.Points ~
+   #                          Actual.Points)
+  #reg.eq <- paste("Y = ", round(Fantasy.Actual.reg$coefficients[1], 2), " + ", 
+   #               round(Fantasy.Actual.reg$coefficients[2], 2), " * X", sep = '')
+  
+  ggplot(data = dat, aes(x = Actual.Points, y = Offensive.Fantasy.Points)) +
+    geom_point(aes(group = Pos, color = Pos), size = 0.5) + 
+    geom_smooth(aes(group = Pos, color = Pos), method = 'lm', formula = y ~ x) +
+    ggtitle(paste("[TBD]", toupper(uniq.teams[i]), sep = " ")) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    xlab("Actual Points") +
+    ylab("Total Offensive Fantasy Points") +
+    #geom_text(x = 10, y = max(Team.Fantasy.totals.Pos.2016$Offensive.Fantasy.Points), label = reg.eq,
+    #         color = 'blue', size = 5) +
+    scale_x_continuous(limits = c(-2, max(Team.Fantasy.totals.Pos.2016$Actual.Points) + 3)) +
+    scale_y_continuous(limits = c(min(Team.Fantasy.totals.Pos.2016$Offensive.Fantasy.Points - 5),
+                                  max(Team.Fantasy.totals.Pos.2016$Offensive.Fantasy.Points + 5)))
+  ggsave(paste("Visualizations/Ch_2/By Position/Team viz/Actual.vs.Fantasy.Points_",
+               toupper(uniq.teams[i]), ".png", sep =''))
+}
